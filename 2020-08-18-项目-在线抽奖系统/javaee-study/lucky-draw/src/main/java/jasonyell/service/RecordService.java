65b70e9fd734b0b5a7d82e34e91bb40fbec6a063
jasonyell <65152013+jasonyell@users.noreply.github.com>
2021-04-06 20:03:09 +0800

    Delete 2020-08-18-项目-在线抽奖系统/javaee-study directory

--- 2020-08-18-项目-在线抽奖系统/javaee-study/lucky-draw/src/main/java/jasonyell/service/RecordService.java
+++
@@ -1,7 +0,0 @@
-package jasonyell.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class RecordService {
-}
--- 2020-08-18-项目-在线抽奖系统/javaee-study/lucky-draw/src/test/java/jasonyell/mapper/RecordMapper.java
+++
@@ -1,9 +0,0 @@
-package jasonyell.mapper;
-
-import jasonyell.base.BaseMapper;
-import jasonyell.model.Record;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RecordMapper extends BaseMapper<Record> {
-}
